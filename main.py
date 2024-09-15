80ab9130f9f227399963c2fab1f86db1888cff24
quangnhat22 <wuangsotech@gmail.com>
2024-09-15 16:00:34 +0700

    push code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    n = int(input("Nhap so n: "))
+    sum = n*(n+1)//2
+    print(sum)
+
+if __name__ == "__main__":
+    main()
+
+
+    
---
+++ sort.py
@@ -0,0 +1,52 @@
+# selection sort
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+# insertion sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+# quick sort
+def partition(arr, left, right):
+    pivot = arr[right]
+    i = left
+    for j in range(left, right):
+        if arr[j] < pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[right] = arr[right], arr[i + 1]
+    return i
+
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot = partition(arr, low, high)
+        quick_sort(arr, low, pivot - 1)
+        quick_sort(arr, pivot + 1, high)
+
+
+# merge sort
+
+
+def main():
+    input = [1, 5, 0, 2, -1, 0, 10, 1, -20, -20, 3]
+    quick_sort(input, 0, len(input) - 1)
+    for i in input:
+        print(i, end=" ")
+
+
+if __name__ == '__main__':
+    main()
