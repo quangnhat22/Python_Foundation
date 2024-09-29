4f905f50875bd5ca3182822a0d99c258947d1e2f
Nguyễn Đình Nhật Quang <quang@gmail.com>
2024-09-29 10:53:45 +0700

    update code

---
+++ tries.py
@@ -0,0 +1,59 @@
+Trie = {
+    "c": {
+        "a": {
+            "t": {
+                "endWord": True,
+            },
+            "r": {
+                "endWord": True,
+            },
+            "endWord": False,
+        },
+        "o": {
+            "w": {
+                "endWord": True,
+            },
+            "endWord": False,
+        },
+        "endWord": False,
+    },
+    "b": {
+        "a": {
+            "n": {
+                "a": {
+                    "n": {
+                        "a": {
+                            "endWord": True,
+                        },
+                        "endWord": False,
+                    },
+                    "endWord": False,
+                },
+                "endWord": False,
+            },
+            "endWord": False,
+        },
+        "endWord": False,
+    },
+    "a": {
+        "l": {
+            "i": {
+                "b": {
+                    "a": {
+                        "b": {
+                            "a": {
+                                "endWord": True,
+                            },
+                            "endWord": False,
+                        },
+                        "endWord": False,
+                    },
+                    "endWord": False,
+                },
+                "endWord": False,
+            },
+            "endWord": False,
+        },
+        "endWord": False,
+    }
+}
